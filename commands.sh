6ac7e39c39ac8e3af87aff2e6a533b907af07f66
rishavmajumdar <rishav@xeno.in>
2024-08-30 12:22:51 +0530

    Modified dockerfile and added spark script

--- commands.sh
+++ commands.sh
@@ -3,4 +3,4 @@ docker exec -it spark-cluster-spark-master-1 /bin/sh
 
 
 # submit spark job
-docker exec -it spark-cluster-spark-master-1 /spark/bin/spark-submit /scripts/spark_job.py
+docker exec -it spark-cluster-spark-master-1 /spark/bin/spark-submit /scripts/simple_spark_job.py
